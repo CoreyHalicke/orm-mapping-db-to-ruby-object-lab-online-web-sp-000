903da34199ebe5508ddbc584f7c68d2cbd558511
Corey H <c.l.halicke@gmail.com>
2019-08-31 01:32:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,6 +15,8 @@ class Student
       FROM students
     SQL
 
+binding.pry
+    
     DB[:conn].execute(sql)
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
